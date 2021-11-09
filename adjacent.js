afead78bb975ab301dd3e4c376e97bd12db99239
vivianzhou1288 <87792254+vivianzhou1288@users.noreply.github.com>
2021-11-09 09:47:38 -0500

    adjacent HTML

--- adjacent.js
+++ adjacent.js
@@ -3,4 +3,10 @@ const DOMSelectors = {
   box: document.getElementById("container-box"),
   input: document.querySelector(`#input`),
 };
-DOMSelectors.box.insertAdjacentHTML();
+/* const cat = "meow";
+DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are an ${cat} </h1>`);
+ */
+DOMSelectors.button.addEventListener("click", function () {
+  let input = DOMSelectors.input.value;
+  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+});
